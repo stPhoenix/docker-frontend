da289510b3231874e0cae92372b9d1a958fa0804
ST Liar <bohdansapovsky@gmail.com>
2021-07-07 21:51:00 +0300

    fix pagination

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -24,7 +24,13 @@ class UsersContainer extends Component {
             .then((api_response)=>{
                 if (api_response)
                 {
-                    this.setState({user_list: api_response.data.results, next: api_response.data.next, previous:api_response.data.previous})
+                    const regex = /(?<=page\=)\d+/gm;
+                    let found_prev = api_response.data.previous != null ? api_response.data.previous.match(regex) : null
+                    let found_next = api_response.data.next != null ? api_response.data.next.match(regex) : null
+                    let previous = found_prev ? found_prev[0] : null
+                    let next = found_next ? found_next[0] : null
+
+                    this.setState({user_list: api_response.data.results, next, previous})
                 }
                 else {
                     this.props.addAlert({variant:"danger", text:api_response.message})
