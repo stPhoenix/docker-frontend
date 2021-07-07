b58450d9fdbaa0bd036da8739bc5fa84a5130ac6
ST Liar <bohdansapovsky@gmail.com>
2021-07-07 22:08:44 +0300

    refaktor pagination

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -3,16 +3,18 @@ import { connect } from 'react-redux';
 import { get_user_list } from '../api'
 import {add as addAlert} from "../slices/alerts";
 import {UsersComponent} from "../components/UsersComponent";
+import { fetch_data_page } from '../tools/fetch_data_page';
 
 class UsersContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            user_list: [],
+            fetched_list: [],
             next: null,
             previous: null
         }
         this.fetch_users = this.fetch_users.bind(this)
+        this.setState = this.setState.bind(this)
     }
 
     componentDidMount() {
@@ -20,28 +22,13 @@ class UsersContainer extends Component {
     }
 
     fetch_users(page) {
-        get_user_list(this.props.tokens, {page})
-            .then((api_response)=>{
-                if (api_response)
-                {
-                    const regex = /(?<=page\=)\d+/gm;
-                    let found_prev = api_response.data.previous != null ? api_response.data.previous.match(regex) : null
-                    let found_next = api_response.data.next != null ? api_response.data.next.match(regex) : null
-                    let previous = found_prev ? found_prev[0] : null
-                    let next = found_next ? found_next[0] : null
-
-                    this.setState({user_list: api_response.data.results, next, previous})
-                }
-                else {
-                    this.props.addAlert({variant:"danger", text:api_response.message})
-                }
-            })
+        fetch_data_page(this.setState, get_user_list, this.props.tokens, page, this.props.addAlert)
     }
 
 
     render() {
         return (
-            <UsersComponent user_list={this.state.user_list} previous={this.state.previous} next={this.state.next} paginate={this.fetch_users} />
+            <UsersComponent user_list={this.state.fetched_list} previous={this.state.previous} next={this.state.next} paginate={this.fetch_users} />
         )
     }
 
---
+++ src/tools/fetch_data_page.js
@@ -0,0 +1,20 @@
+export const fetch_data_page = (setState, apiCall, tokens, page, addAlert) => {
+    apiCall(tokens, {page})
+    .then((api_response)=>{
+        if (api_response)
+        {
+            const regex = /(?<=page=)\d+/gm;
+            let found_prev = api_response.data.previous != null ? api_response.data.previous.match(regex) : null
+            let found_next = api_response.data.next != null ? api_response.data.next.match(regex) : null
+            let previous = found_prev ? found_prev[0] : null
+            let next = found_next ? found_next[0] : null
+            previous = next == "3" ? "1" : previous
+
+            setState({fetched_list: api_response.data.results, next, previous})
+        }
+        else {
+            addAlert({variant:"danger", text:api_response.message})
+        }
+    })
+
+}
