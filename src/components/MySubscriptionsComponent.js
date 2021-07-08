191eab1d9c178a8b254b79f290a76c23c65d07ae
Bogdan Sapovskyi <b.sapobskyi@quantumobile.com>
2021-07-08 15:08:42 +0300

    updated my subscriptions component

--- src/components/MySubscriptionsComponent.js
+++ src/components/MySubscriptionsComponent.js
@@ -12,6 +12,7 @@ export const MySubscriptionsComponent = (props) => {
             <br/>
                 <Table>
                     <thead>
+                        <th>Created</th>
                         <th>Target</th>
                         <th>Status</th>
                         <th>Actions</th>
@@ -21,8 +22,9 @@ export const MySubscriptionsComponent = (props) => {
                             props.subscriptions_list.map(subscription => {
                                 return(
                                     <tr key={subscription.id}>
-                                        <td>{subscription.target}</td>
-                                        <td>{subscription.status}</td>
+                                        <td>{subscription.created}</td>
+                                        <td>{subscription.target_username}</td>
+                                        <td>{subscription.status_text}</td>
                                         <td><Button disabled={subscription.status !== 1} value={subscription.id} variant="outline-primary" onClick={props.onClick}>Abort request</Button></td>
                                     </tr>
                                 )
